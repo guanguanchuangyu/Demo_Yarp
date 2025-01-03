689c9d88ef6a0a222a9de7a3f577fcc6155a634a
ggcy <3246907890@qq.com>
2025-01-03 23:48:29 +0800

    [配置文件]-多配置应用

--- MyProxy/Program.cs
+++ MyProxy/Program.cs
@@ -1,10 +1,12 @@
 
+using Yarp.ReverseProxy.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 // 添加Yarp中间件
 IReverseProxyBuilder reverseProxyBuilder = builder.Services.AddReverseProxy();
-// 添加配置文件
+// 多配置源
 reverseProxyBuilder.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
+reverseProxyBuilder.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy1"));
 var app = builder.Build();
 // 添加代理路由到管道内路由表中
 app.MapReverseProxy();
