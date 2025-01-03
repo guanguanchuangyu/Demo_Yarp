9e28569a7b986bab00bdea460b308997b170d2ff
ggcy <3246907890@qq.com>
2025-01-03 23:09:18 +0800

    [快速开始]-读取配置所有请求代理到百度

--- MyProxy/Program.cs
+++ MyProxy/Program.cs
@@ -1,8 +1,12 @@
 
 var builder = WebApplication.CreateBuilder(args);
 // 添加Yarp中间件
-builder.Services.AddReverseProxy();
+IReverseProxyBuilder reverseProxyBuilder = builder.Services.AddReverseProxy();
+// 添加配置文件
+reverseProxyBuilder.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
+// 添加代理路由到管道内路由表中
+app.MapReverseProxy();
 
 app.Run();
